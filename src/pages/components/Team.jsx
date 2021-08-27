7ece907148a27f735aa388a7e34aa291e89851a2
shubham-ghuge <shubhamghuge34@gmail.com>
2021-08-27 21:10:23 +0530

    fixed bg

--- src/pages/components/Team.jsx
+++ src/pages/components/Team.jsx
@@ -3,30 +3,64 @@ import { TeamCard } from "./TeamCard";
 
 function Team() {
   const data1 = [
-    { id: "001", name: "Freddy_Smith", title: "CEO and Founder", bg: "green" },
-    { id: "002", name: "Carl_Jones", title: "CTO and Co-Founder", bg: "blue" },
+    {
+      id: "001",
+      name: "Freddy_Smith",
+      title: "CEO and Founder",
+      bg: "rgba(209, 250, 229, 1)",
+    },
+    {
+      id: "002",
+      name: "Carl_Jones",
+      title: "CTO and Co-Founder",
+      bg: "rgba(219, 234, 254, 1)",
+    },
     {
       id: "003",
       name: "Susan_Peterson",
       title: "Marketing Directory",
-      bg: "red",
+      bg: "rgba(254, 226, 226, 1)",
     },
-    { id: "004", name: "Tommy_Barnes", title: "Designer", bg: "indigo" },
-    { id: "001", name: "Ron_Jenson", title: "Senior Developer", bg: "indigo" },
-    { id: "002", name: "Pete_Tompkins", title: "Web Developer", bg: "red" },
-    { id: "003", name: "Kelly_Richards", title: "Sales Manager", bg: "blue" },
+    {
+      id: "004",
+      name: "Tommy_Barnes",
+      title: "Designer",
+      bg: "rgba(224, 231, 255, 1)",
+    },
+    {
+      id: "001",
+      name: "Ron_Jenson",
+      title: "Senior Developer",
+      bg: "rgba(224, 231, 255, 1)",
+    },
+    {
+      id: "002",
+      name: "Pete_Tompkins",
+      title: "Web Developer",
+      bg: "rgba(254, 226, 226, 1)",
+    },
+    {
+      id: "003",
+      name: "Kelly_Richards",
+      title: "Sales Manager",
+      bg: "rgba(219, 234, 254, 1)",
+    },
     {
       id: "004",
       name: "Alexis_Jordan",
       title: "Affiliate Manager",
-      bg: "green",
+      bg: "rgba(209, 250, 229, 1)",
     },
   ];
   return (
-    <section className="mx-4 mt-16 sm:w-11/12 sm:px-4 sm:mx-auto relative" id="team">
+    <section
+      className="mx-4 mt-16 sm:w-11/12 sm:px-4 sm:mx-auto relative"
+      id="team"
+    >
       <p className="text-indigo-600 text-md sm:text-xl sm:mb-6">OUR TEAM</p>
       <h2 className="text-3xl font-semibold sm:text-5xl">
-        An incredible team of <br className="hidden sm:block"/> amazing individuals
+        An incredible team of <br className="hidden sm:block" /> amazing
+        individuals
       </h2>
       <div className="hidden absolute top-0 rounded-tl-full rounded-bl-full -right-14 sm:block h-32 w-2/5 bg-blue-50"></div>
       <div className="flex mt-8 sm:mb-10 justify-center flex-wrap sm:justify-between sm:mt-16">
--- src/pages/components/TeamCard.jsx
+++ src/pages/components/TeamCard.jsx
@@ -5,10 +5,15 @@ function TeamCard({ details }) {
   const { name, title, bg } = details;
   return (
     <div className="flex flex-col mx-1 items-center mb-6 sm:mb-12">
-      <div className={`bg-${bg}-100 p-3 sm:p-5 mx-2 rounded-tl-full rounded-b-full`}>
+      <div
+        className={`p-3 sm:p-5 mx-2 rounded-tl-full rounded-b-full`}
+        style={{ background: bg }}
+      >
         <img src={`assets/team/${name}.png`} className="h-28 sm:h-52" alt="" />
       </div>
-      <p className="text-md mt-2 sm:text-xl sm:mt-6">{name.split("_").join(" ")}</p>
+      <p className="text-md mt-2 sm:text-xl sm:mt-6">
+        {name.split("_").join(" ")}
+      </p>
       <p className="text-indigo-600 text-md sm:text-xl">{title}</p>
       <div className="flex justify-center mt-2 text-gray-400 text-xl">
         <FaTwitter className="mr-2" />
